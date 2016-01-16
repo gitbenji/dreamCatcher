4c61fe4a4c41d65afaec9c342089142ea3f27ff9
Benji Shankwitz <benji@Benjis-MacBook-Pro.local>
2016-01-16 13:45:58 -0500

    concatenation

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 
-'use strict';
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -8,6 +7,7 @@ var port = 6000;
 var app = {};
 var child_process = require('child_process').exec;
 var ffmpeg = require('fluent-ffmpeg');
+var fs = require('fs');
 
 
 var e = app.e = express();
@@ -31,29 +31,45 @@ app.server.listen(port, function() {
 //-----------------------------------CONCATENATE THAT VIDEO SHIT--------------------------------------------//
 
 //testing purposes
-var arr=['../videoslicer/"dog slices"/1368547613.mp4','../videoslicer/"dog slices"/1814283056.mp4','../videoslicer/"dog slices"/1386206500.mp4','../videoslicer/"dog slices"/1706709066.mp4','../videoslicer/"dog slices"/1726081069.mp4','../videoslicer/"dog slices"/5044944993.mp4','../videoslicer/"dog slices"/4859706534.mp4']
-concat(arr);
+var vidarr=['../videoslicer/"dog slices"/1368547613.mp4','../videoslicer/"dog slices"/1814283056.mp4','../videoslicer/"dog slices"/1386206500.mp4','../videoslicer/"dog slices"/1706709066.mp4','../videoslicer/"dog slices"/1726081069.mp4','../videoslicer/"dog slices"/5044944993.mp4','../videoslicer/"dog slices"/4859706534.mp4']
+receive(vidarr);
 //----------------
 
-function concat(video) {
-	console.log(video.length-1);
-	for(var i = 0; i < video.length; i++) {
-		child_process('ffmpeg -i ' + video[i] + ' -qscale:v 1 intermediate'+[i]+'.mpg', function (err, data) {
-			//console.log(video[i]);
-		});
-		if (i === video.length-1) {
-			//console.log('call merge');
-			merge(video);
+//create array of random integers for unique temporary file names
+function receive(videos) {
+	var temparr = [];
+	child_process('mkdir tempVids', function (err, data) {
+		// console.log('tempVids made');
+	})
+	for(var i = 0; i < videos.length; i++) {
+		temparr[i] = Math.floor(Math.random() * (9999999999 - 1000000000 +1)) + 1000000000
+		if (i === videos.length-1) {
+			concat(videos, temparr);
 		}
 	}
 }
 
-function merge(video) {
-	console.log('merge');
+function concat(videos, temparr) {
+	console.log(videos.length-1);
+	for(var i = 0; i < videos.length; i++) {
+		(function(index){
+			child_process('ffmpeg -i ' + videos[index] + ' -qscale:v 1 ./tempVids/' + temparr[index] + '.mpg', function (err, data) {
+				//console.log(video[i]);
+				if (index === videos.length-1) {
+					//console.log('call merge');
+					merge(videos, temparr);
+				}
+			});
+		})(i);
+	}
+}
+
+function merge(videos, temparr) {
+	// console.log('merge');
 	var string = "cat ";
-	for (var i = 0; i < video.length; i++){
-		string += "intermediate"+[i]+".mpg "
-		if (i === video.length-1){
+	for (var i = 0; i < videos.length; i++){
+		string += './tempVids/'+temparr[i]+".mpg "
+		if (i === videos.length-1) {
 			// console.log('execute');
 			execute(string);
 		}
@@ -71,22 +87,38 @@ function execute(string) {
 
 function finish() {
 	child_process('ffmpeg -i intermediate_all.mpg -qscale:v 2 output.mp4', function (err, data) {
+		console.log(err);
     	console.log('done, bitch');
-    	// console.log(data);
+    	console.log(data);
     });
+    deleteTemps();
 }
 
+function deleteTemps() {
+	child_process('rm -r ./tempVids', function(err){
+		if (err)
+			console.log(err);
+	});
+}
+
+// function deleteTemps() {
+// 	fs.unlink('./tempVids/', function(err){
+// 		console.log(err);
+// 		console.log('deleted?')
+// 	});
+// }
 
 //------------------------------------STOP CONCATENATING BECAUSE YOU FUCKING DID IT-----------------------------------//
+
 //------------------------------------THUMBNAIL, YO-------------------------------------------------------------------//
 
-// var proc = ffmpeg('output.mp4')
-// 	.takeScreenshots({
-// 		count: 1,
-// 		timemarks: [ '4' ]
-// 	}, '/', function(err) {
-// 		console.log('screenshots were saved');
-// 	});
+var proc = ffmpeg('output.mp4')
+	.takeScreenshots({
+		count: 1,
+		timemarks: [ '2' ]
+	}, '/', function(err) {
+		console.log('screenshots were saved');
+	});
 
 
 //------------------------------------THUMBNAIL, YO-------------------------------------------------------------------//
